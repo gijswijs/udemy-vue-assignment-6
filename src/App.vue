6d0402b7984f30c2c38e266db70cd328fd6070f5
Gijs van Dam <gvandam@gmail.com>
2018-03-20 15:05:38 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-server-list></app-server-list>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
---
+++ src/ServerList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server-status v-for="(server, index) in 5" :key="index" :index="index"></app-server-status>
+        </ul>
+    </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus'
+export default {
+    components: {
+        'app-server-status' : ServerStatus
+    }
+}
+</script>
+
+<style>
+
+</style>
